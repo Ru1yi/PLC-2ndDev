91ba654a5e024d95a38730b2b7dc2642861d227a
Ru1yi <waterwinsor@gmail.com>
2024-11-01 14:54:20 +0800

    feat: add release config of vs

--- Simens-S7/main.cpp
+++ Simens-S7/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char *argv[])
    GlobalPlc = new PlcControl();
     bool ret = GlobalPlc->PlcConnectInit();
 
-    GlobalPlc->DBWrite_Int(1080, 260, 25);
+    GlobalPlc->DBWrite_Int(1080, 264, 0);
 
-    //int xintiao = GlobalPlc->DBRead_Int(1080, 260);
+    //int xintiao = GlobalPlc->DBRead_Int(1080, 268);
     
     //GlobalPlc->DBWrite_String(1080, 0, "BYD12345", 50);
 
